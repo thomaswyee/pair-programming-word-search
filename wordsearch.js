a8246515520961d5dc940ace13537075c3708113
Your Name <thomas.w.yee@gmail.com>
2021-01-14 08:32:44 -0500

    util refactor

--- wordsearch.js
+++ wordsearch.js
@@ -24,7 +24,7 @@ const wordSearch = (letters, word) => {
       return true;
     }
   }
-  if (diag(letters, word)) {
+  if (diag(letters, word, wordReverse)) {
     return true;
   }
   return false;
